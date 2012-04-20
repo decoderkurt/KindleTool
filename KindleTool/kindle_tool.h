e7ee5703dd6315e66bbcba432c48e10cf083090f
NiLuJe <ninuje@gmail.com>
2012-04-20 05:59:45 +0200

    Wee, annoying whitespace removal commit, part II!

--- KindleTool/kindle_tool.h
+++ KindleTool/kindle_tool.h
@@ -3,17 +3,17 @@
 //  KindleTool
 //
 //  Copyright (C) 2011  Yifan Lu
-//  
+//
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
@@ -145,7 +145,7 @@ typedef struct {
     char **metastrings;
 } UpdateInformation;
 
-static const char SIGN_KEY[] = 
+static const char SIGN_KEY[] =
     "-----BEGIN RSA PRIVATE KEY-----\n"
     "MIICXgIBAAKBgQDJn1jWU+xxVv/eRKfCPR9e47lPWN2rH33z9QbfnqmCxBRLP6mM\n"
     "jGy6APyycQXg3nPi5fcb75alZo+Oh012HpMe9LnpeEgloIdm1E4LOsyrz4kttQtG\n"
