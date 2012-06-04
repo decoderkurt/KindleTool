e78c925e8cb0fa01e4c17667f93a91bd913adea6
NiLuJe <ninuje@gmail.com>
2012-06-04 22:04:13 +0200

    Fix a typo in the docs

--- KindleTool/kindle_tool.c
+++ KindleTool/kindle_tool.c
@@ -324,7 +324,7 @@ int kindle_print_help(const char *prog_name)
         "    \n"
         "notices:\n"
         "  1)  Kindle 4.0+ has a known bug that prevents some updates with meta-strings to run.\n"
-        "  2)  Currently, even though OTA V2 supports updates that run on multiple devices, it is not possible to create a update package that will run on both the Kindle 4 (No Touch) and Kindle 5 (Kindle Touch).\n"
+        "  2)  Currently, even though OTA V2 supports updates that run on multiple devices, it is not possible to create an update package that will run on both the Kindle 4 (No Touch) and Kindle 5 (Kindle Touch).\n"
         , prog_name, prog_name, prog_name, prog_name, prog_name, prog_name, prog_name, prog_name);
     return 0;
 }
