bb08362ad63bc853b83ec869c1e94b37d208713d
NiLuJe <ninuje@gmail.com>
2012-06-04 04:36:09 +0200

    static buildscript: Download stuff if it's missing, zip the packages on OS X, and support building x86_64 Linux packages.

--- kindletool-static-build.sh
+++ kindletool-static-build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 OSTYPE="$(uname -s)"
+ARCH="$(uname -m)"
 
 ## Linux!
 Build_Linux() {
 	echo "* Preparing a static KindleTool build on Linux . . ."
-	export CFLAGS="-march=i686 -pipe -O2 -fomit-frame-pointer -fno-stack-protector -U_FORTIFY_SOURCE"
-	export CXXFLAGS="-march=i686 -pipe -O2 -fomit-frame-pointer -fno-stack-protector -U_FORTIFY_SOURCE"
+	if [[ "${ARCH}" == "x86_64" ]] ; then
+		export CFLAGS="-march=core2 -pipe -O2 -fomit-frame-pointer -fno-stack-protector -U_FORTIFY_SOURCE"
+		export CXXFLAGS="-march=core2 -pipe -O2 -fomit-frame-pointer -fno-stack-protector -U_FORTIFY_SOURCE"
+	else
+		export CFLAGS="-march=i686 -pipe -O2 -fomit-frame-pointer -fno-stack-protector -U_FORTIFY_SOURCE"
+		export CXXFLAGS="-march=i686 -pipe -O2 -fomit-frame-pointer -fno-stack-protector -U_FORTIFY_SOURCE"
+	fi
 	export LDFLAGS="-Wl,-O1 -Wl,--as-needed"
 
 	OPENSSL_DIR="openssl-0.9.8x"
@@ -23,7 +29,10 @@ Build_Linux() {
 		echo "* Building ${OPENSSL_DIR} . . ."
 		echo ""
 		export LDFLAGS="-Wa,--noexecstack"
-		tar -xvzf /usr/portage/distfiles/${OPENSSL_DIR}.tar.gz
+		if [[ ! -f "./${OPENSSL_DIR}.tar.gz" ]] ; then
+			wget -O "http://www.openssl.org/source/${OPENSSL_DIR}.tar.gz" "./${OPENSSL_DIR}.tar.gz"
+		fi
+		tar -xvzf ./${OPENSSL_DIR}.tar.gz
 		cd ${OPENSSL_DIR}
 		patch -p1 < /usr/portage/dev-libs/openssl/files/openssl-0.9.8e-bsd-sparc64.patch
 		patch -p1 < /usr/portage/dev-libs/openssl/files/openssl-0.9.8h-ldflags.patch
@@ -49,7 +58,10 @@ Build_Linux() {
 		echo ""
 		export ac_cv_header_ext2fs_ext2_fs_h=0
 		export LDFLAGS="-Wl,-O1 -Wl,--as-needed"
-		tar -xvzf /usr/portage/distfiles/${LIBARCHIVE_DIR}.tar.gz
+		if [[ ! -f "./${LIBARCHIVE_DIR}.tar.gz" ]] ; then
+			wget -O "./${LIBARCHIVE_DIR}.tar.gz" "https://github.com/downloads/libarchive/libarchive/${LIBARCHIVE_DIR}.tar.gz"
+		fi
+		tar -xvzf ./${LIBARCHIVE_DIR}.tar.gz
 		cd ${LIBARCHIVE_DIR}
 		./configure --enable-static --disable-shared --disable-xattr --disable-acl --with-zlib --without-bz2lib --without-lzmadec --without-iconv --without-lzma --without-nettle --without-expat --without-xml2
 		make
@@ -72,12 +84,16 @@ EOF
 	echo ""
 	export LDFLAGS="-Llib -Wl,-O1 -Wl,--as-needed"
 	cd KindleTool/KindleTool
-	rm -rf lib
-	mkdir -p lib
-	cp ../../${LIBARCHIVE_DIR}/.libs/libarchive.a lib
+	rm -rf lib includes
+	mkdir -p lib includes
+	cp -v ../../${LIBARCHIVE_DIR}/.libs/libarchive.a lib
+	if [[ "${ARCH}" == "x86_64" ]] ; then
+		cp -v ../../${LIBARCHIVE_DIR}/libarchive/archive.h includes
+		cp -v ../../${LIBARCHIVE_DIR}/libarchive/archive_entry.h includes
+	fi
 	make clean
 	make strip
-	rm -rf lib
+	rm -rf lib includes
 
 	# Package it
 	git log --stat --graph > ../../ChangeLog
@@ -92,7 +108,7 @@ EOF
 	sed -si 's/&lt;/</g;s/&gt;/>/g;s/&amp;/&/g;s/^* /  /g;s/*//g;s/>> /\t/g;s/^> /  /g;s/^## //g;s/### //g;s/\t/    /g;s/^\([[:digit:]]\)\./  \1)/g;s/^#.*$//;s/[[:blank:]]*$//g' README
 	cp -v KindleTool/KindleTool/kindletool.1 ./kindletool.1
 	mv -v KindleTool/KindleTool/VERSION ./VERSION
-	tar -cvzf kindletool-${REV}-static.tar.gz kindletool CREDITS README kindletool.1 ChangeLog VERSION
+	tar -cvzf kindletool-${REV}-static-${ARCH}.tar.gz kindletool CREDITS README kindletool.1 ChangeLog VERSION
 	rm -f kindletool README kindletool.1 ChangeLog VERSION
 
 
@@ -100,16 +116,20 @@ EOF
 	echo "* Building KindleTool (OpenSSL-0.9.8) . . ."
 	echo ""
 	cd KindleTool/KindleTool
-	rm -rf lib
-	mkdir -p lib
-	cp ../../${LIBARCHIVE_DIR}/.libs/libarchive.a lib
-	cp ../../${OPENSSL_DIR}/libcrypto.so.0.9.8 lib
+	rm -rf lib includes
+	mkdir -p lib includes
+	cp -v ../../${LIBARCHIVE_DIR}/.libs/libarchive.a lib
+	cp -v ../../${OPENSSL_DIR}/libcrypto.so.0.9.8 lib
 	cd lib
 	ln -sf libcrypto.so.0.9.8 libcrypto.so
 	cd ..
+	if [[ "${ARCH}" == "x86_64" ]] ; then
+		cp -v ../../${LIBARCHIVE_DIR}/libarchive/archive.h includes
+		cp -v ../../${LIBARCHIVE_DIR}/libarchive/archive_entry.h includes
+	fi
 	make clean
 	make strip
-	rm -rf lib
+	rm -rf lib includes
 
 	# Package it
 	git log --stat --graph > ../../ChangeLog
@@ -124,7 +144,7 @@ EOF
 	sed -si 's/&lt;/</g;s/&gt;/>/g;s/&amp;/&/g;s/^* /  /g;s/*//g;s/>> /\t/g;s/^> /  /g;s/^## //g;s/### //g;s/\t/    /g;s/^\([[:digit:]]\)\./  \1)/g;s/^#.*$//;s/[[:blank:]]*$//g' README
 	cp -v KindleTool/KindleTool/kindletool.1 ./kindletool.1
 	mv -v KindleTool/KindleTool/VERSION ./VERSION
-	tar -cvzf kindletool-${REV}-static-openssl-0.9.8.tar.gz kindletool CREDITS README kindletool.1 ChangeLog VERSION
+	tar -cvzf kindletool-${REV}-static-${ARCH}-openssl-0.9.8.tar.gz kindletool CREDITS README kindletool.1 ChangeLog VERSION
 	rm -f kindletool CREDITS README kindletool.1 ChangeLog VERSION
 }
 
@@ -147,7 +167,10 @@ Build_Cygwin() {
 	if [[ ! -d "${LIBARCHIVE_DIR}" ]] ; then
 		echo "* Building ${LIBARCHIVE_DIR} . . ."
 		echo ""
-		tar -xvzf ~/${LIBARCHIVE_DIR}.tar.gz
+		if [[ ! -f "./${LIBARCHIVE_DIR}.tar.gz" ]] ; then
+			wget -O "./${LIBARCHIVE_DIR}.tar.gz" "https://github.com/downloads/libarchive/libarchive/${LIBARCHIVE_DIR}.tar.gz"
+		fi
+		tar -xvzf ./${LIBARCHIVE_DIR}.tar.gz
 		cd ${LIBARCHIVE_DIR}
 		./configure --prefix=/usr --enable-static --enable-shared --disable-xattr --disable-acl --with-zlib --without-bz2lib --without-lzmadec --without-iconv --without-lzma --without-nettle --without-expat --without-xml2
 		make
@@ -218,7 +241,10 @@ Build_OSX() {
 	if [[ ! -d "${LIBARCHIVE_DIR}" ]] ; then
 		echo "* Building ${LIBARCHIVE_DIR} . . ."
 		echo ""
-		tar -xvzf ~/${LIBARCHIVE_DIR}.tar.gz
+		if [[ ! -f "./${LIBARCHIVE_DIR}.tar.gz" ]] ; then
+			curl -L "https://github.com/downloads/libarchive/libarchive/${LIBARCHIVE_DIR}.tar.gz" -o "./${LIBARCHIVE_DIR}.tar.gz"
+		fi
+		tar -xvzf ./${LIBARCHIVE_DIR}.tar.gz
 		cd ${LIBARCHIVE_DIR}
 		./configure --prefix=/opt/local --enable-static --enable-shared --disable-xattr --disable-acl --with-zlib --without-bz2lib --without-lzmadec --without-iconv --without-lzma --without-nettle --without-expat --without-xml2
 		make
@@ -244,7 +270,7 @@ EOF
 	cd KindleTool/KindleTool
 	rm -rf lib
 	mkdir -p lib
-	cp ../../${LIBARCHIVE_DIR}/.libs/libarchive.a lib
+	cp -v ../../${LIBARCHIVE_DIR}/.libs/libarchive.a lib
 	make clean
 	make strip
 	rm -rf lib
@@ -263,7 +289,9 @@ EOF
 	perl -pi -e 's/&lt;/</g;s/&gt;/>/g;s/&amp;/&/g;s/^\* /  /g;s/\*//g;s/>> /\t/g;s/^> /  /g;s/^## //g;s/### //g;s/\t/    /g;s/^([[:digit:]])\./  \1)/g;s/^#.*$//;s/[[:blank:]]*$//g' ./README
 	cp -v ../KindleTool/KindleTool/KindleTool.1 ./KindleTool.1
 	mv -v ../KindleTool/KindleTool/VERSION ./VERSION
-	tar -cvzf kindletool-${REV}-osx.tar.gz kindletool CREDITS README KindleTool.1 ChangeLog VERSION
+	rm -f kindletool-${REV}-osx.zip
+	# Don't store uid/gid & attr, I'm packaging this on a 3rd party's computer
+	zip -X kindletool-${REV}-osx kindletool CREDITS README KindleTool.1 ChangeLog VERSION
 	rm -f kindletool CREDITS README KindleTool.1 ChangeLog VERSION
 	cd ..
 
