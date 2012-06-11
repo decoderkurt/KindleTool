e4832261d780b5d2feba8c0c5a7af9d8903c37b5
NiLuJe <ninuje@gmail.com>
2012-06-11 21:46:48 +0200

    Don't delete tarball on error if we asked to keep it

--- KindleTool/create.c
+++ KindleTool/create.c
@@ -1243,8 +1243,8 @@ do_error:
         fclose(bundlefile);
         unlink(bundle_filename);
     }
-    // Remove broken tarball, if we built one
-    if(tarball_filename != NULL && !skip_archive)
+    // Remove broken tarball, if we built one, and we didn't ask to keep it
+    if(tarball_filename != NULL && !skip_archive && !keep_archive)
         unlink(tarball_filename);
     free(tarball_filename);
     return -1;
