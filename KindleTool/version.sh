137cf6b3d8f928ba47c3a60857c4a0e7e5e6ce05
NiLuJe <ninuje@gmail.com>
2012-11-09 18:07:38 +0100

    Bump fallback version to 1.5.5

--- KindleTool/version.sh
+++ KindleTool/version.sh
@@ -4,7 +4,7 @@
 VER_FILE="version-inc"
 
 # Fallback version
-FALLBACK_VER="v1.5.4-GIT"
+FALLBACK_VER="v1.5.5-GIT"
 
 # Apparently, bsdmake hates me, so, get uname's output from here
 UNAME="$(uname -s)"
