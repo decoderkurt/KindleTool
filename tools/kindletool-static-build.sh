69e8b6aa6cb0c37858161c1ef4ed498cd2013381
NiLuJe <ninuje@gmail.com>
2013-11-10 19:29:00 +0100

    Dry-code the OSX support

--- tools/kindletool-static-build.sh
+++ tools/kindletool-static-build.sh
@@ -74,7 +74,6 @@ Build_Linux() {
 		echo "* Building ${LIBARCHIVE_DIR} . . ."
 		echo ""
 		export ac_cv_header_ext2fs_ext2_fs_h=0
-		export LDFLAGS="-Wl,-O1 -Wl,--as-needed"
 		if [[ ! -f "./${LIBARCHIVE_DIR}.tar.gz" ]] ; then
 			wget -O "./${LIBARCHIVE_DIR}.tar.gz" "http://github.com/libarchive/libarchive/archive/v${LIBARCHIVE_VER}.tar.gz"
 		fi
@@ -170,8 +169,8 @@ Build_Cygwin() {
 		tar -xvzf ./${LIBARCHIVE_DIR}.tar.gz
 		cd ${LIBARCHIVE_DIR}
 		# NOTE: The win crypto stuff breaks horribly with the current Cygwin packages...
-		# Switch to cmake, which will properly use OpenSSL on Cygwin, and hope it doesn't break everything, because the tests still fail horribly to build...
-		cmake -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE="Release" -DENABLE_TEST=FALSE -DENABLE_NETTLE=FALSE -DENABLE_XATTR=FALSE -DENABLE_ACL=FALSE -DENABLE_ICONV=FALSE -DENABLE_CPIO=FALSE -DENABLE_TAR=ON -DENABLE_OPENSSL=ON
+		# Switch to cmake, which will properly use Nettle on Cygwin, and hope it doesn't break everything, because the tests still fail horribly to build...
+		cmake -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE="Release" -DENABLE_TEST=FALSE -DENABLE_NETTLE=FALSE -DENABLE_XATTR=FALSE -DENABLE_ACL=FALSE -DENABLE_ICONV=FALSE -DENABLE_CPIO=FALSE -DENABLE_TAR=ON -DENABLE_NETTLE=ON -DENABLE_OPENSSL=FALSE
 		make
 		make install
 		cd ..
@@ -185,16 +184,8 @@ KindleTool, Copyright (C) 2011-2013  Yifan Lu, licensed under the GNU General Pu
 (https://github.com/NiLuJe/KindleTool/)
 
   |
-  |->   libarchive, Copyright (C) Tim Kientzle, licensed under the New BSD License (http://www.opensource.org/licenses/bsd-license.php)
-  |   (http://libarchive.github.com/)
-  |
-  |->   GMP, GNU MP Library, Copyright 1991-2013 Free Software Foundation, Inc.,
-  |   licensed under the GNU Lesser General Public License version 3+ (http://www.gnu.org/licenses/lgpl.html).
-  |   (http://gmplib.org/)
-  |
-  \`->   nettle, Copyright (C) 2001-2013 Niels Möller,
-      licensed under the GNU Lesser General Public License version 2.1+ (https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html).
-      (http://www.lysator.liu.se/~nisse/nettle)
+  \`->   libarchive, Copyright (C) Tim Kientzle, licensed under the New BSD License (http://www.opensource.org/licenses/bsd-license.php)
+      (http://libarchive.github.com/)
 EOF
 
 	# KindleTool
@@ -242,8 +233,11 @@ Build_OSX() {
 	export MACOSX_DEPLOYMENT_TARGET=10.6
 	export CFLAGS="-march=core2 -pipe -O2 -fomit-frame-pointer -mmacosx-version-min=10.6"
 	export CXXFLAGS="-march=core2 -pipe -O2 -fomit-frame-pointer -mmacosx-version-min=10.6"
-	export LDFLAGS=""
 
+	GMP_VER="5.1.3"
+	GMP_DIR="gmp-${GMP_VER}"
+	NETTLE_VER="2.7.1"
+	NETTLE_DIR="nettle-${NETTLE_VER}"
 	LIBARCHIVE_VER="3.1.2"
 	LIBARCHIVE_DIR="libarchive-${LIBARCHIVE_VER}"
 
@@ -252,6 +246,44 @@ Build_OSX() {
 
 	# Get out of our git tree
 	cd ../..
+
+	KT_SYSROOT="${PWD}/kt-sysroot-osx"
+	# NOTE: Use -isystem so that gmp doesn't do crazy stuff...
+	export CPPFLAGS="-isystem${KT_SYSROOT}/include"
+	export LDFLAGS="-L${KT_SYSROOT}/lib"
+
+	# GMP
+	if [[ ! -d "${GMP_DIR}" ]] ; then
+		echo "* Building ${GMP_DIR} . . ."
+		echo ""
+		if [[ ! -f "./${GMP_DIR}.tar.xz" ]] ; then
+			curl -L "http://ftp.gmplib.org/gmp/${GMP_DIR}.tar.xz" -o "./${GMP_DIR}.tar.xz"
+		fi
+		tar -xvJf ./${GMP_DIR}.tar.xz
+		cd ${GMP_DIR}
+		./configure --prefix="${KT_SYSROOT}" --enable-static --disable-shared --disable-cxx
+		make -j2
+		make install
+		cd ..
+	fi
+
+	# nettle
+	if [[ ! -d "${NETTLE_DIR}" ]] ; then
+		echo "* Building ${NETTLE_DIR} . . ."
+		echo ""
+		if [[ ! -f "./${NETTLE_DIR}.tar.gz" ]] ; then
+			curl -L "http://www.lysator.liu.se/~nisse/archive/${NETTLE_DIR}.tar.gz" -o "./${NETTLE_DIR}.tar.gz"
+		fi
+		tar -xvzf ./${NETTLE_DIR}.tar.gz
+		cd ${NETTLE_DIR}
+		sed -e '/CFLAGS=/s: -ggdb3::' -e 's/solaris\*)/sunldsolaris*)/' -i configure.ac
+		sed -i '/SUBDIRS/s/testsuite examples//' Makefile.in
+		autoreconf -fi
+		./configure  --prefix="${KT_SYSROOT}" --enable-static --disable-shared --enable-public-key --disable-openssl --disable-documentation
+		make -j2
+		make install
+		cd ..
+	fi
 
 	# libarchive
 	if [[ ! -d "${LIBARCHIVE_DIR}" ]] ; then
@@ -263,14 +295,14 @@ Build_OSX() {
 		tar -xvzf ./${LIBARCHIVE_DIR}.tar.gz
 		cd ${LIBARCHIVE_DIR}
 		./build/autogen.sh
-		./configure --prefix=/opt/local --enable-static --enable-shared --disable-xattr --disable-acl --with-zlib --without-bz2lib --without-lzmadec --without-iconv --without-lzma --without-nettle --without-expat --without-xml2 --without-openssl
-		make
+		./configure --prefix="${KT_SYSROOT}" --enable-static --disable-shared --disable-xattr --disable-acl --with-zlib --without-bz2lib --without-lzmadec --without-iconv --without-lzma --without-nettle --without-openssl --without-expat --without-xml2
+		make -j2
+		make install
 		cd ..
 	fi
 
 	# Build KT package credits
-	mkdir -p Release
-	cat > Release/CREDITS << EOF
+	cat > CREDITS << EOF
 * kindletool:
 
 KindleTool, Copyright (C) 2011-2013  Yifan Lu, licensed under the GNU General Public License version 3+ (http://www.gnu.org/licenses/gpl.html).
@@ -297,17 +329,10 @@ EOF
 		export KT_NO_USERATHOST_TAG="true"
 		export CFLAGS="-march=core2 -pipe -O2 -fomit-frame-pointer -mmacosx-version-min=10.6 -DKT_USERATHOST='\"niluje@ajulutsikael\"'"
 	fi
-	export CPPFLAGS="-Iincludes"
-	export LDFLAGS="-Llib"
 	cd KindleTool/KindleTool
 	rm -rf lib includes
-	mkdir -p lib includes
-	cp -v ../../${LIBARCHIVE_DIR}/.libs/libarchive.a lib
-	cp -v ../../${LIBARCHIVE_DIR}/libarchive/archive.h includes
-	cp -v ../../${LIBARCHIVE_DIR}/libarchive/archive_entry.h includes
 	make clean
 	make strip
-	rm -rf lib includes
 
 	# Package it
 	git log --stat --graph > ../../Release/ChangeLog
@@ -330,8 +355,6 @@ EOF
 	zip -X kindletool-${REV}-osx.zip kindletool CREDITS README kindletool.1 ChangeLog VERSION
 	rm -f kindletool CREDITS README kindletool.1 ChangeLog VERSION
 	cd ..
-
-	## NOTE: Do we need to do a MacPorts build? (link against MacPorts libs, instead of OS X's OpenSSL 0.9.8/zlib 1.2.5?
 }
 
 # Main
