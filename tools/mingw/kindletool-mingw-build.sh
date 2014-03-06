eb26a2e53e2a405ac84ff55e11b34a18397fc2c8
NiLuJe <ninuje@gmail.com>
2014-03-06 20:14:42 +0100

    Update MinGW TC URL

--- tools/mingw/kindletool-mingw-build.sh
+++ tools/mingw/kindletool-mingw-build.sh
@@ -94,7 +94,7 @@ EOF
 }
 
 ## I'd use this TC: http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/rubenvb/gcc-4.7-release/
-## but they're actually for an x86_64 Linux host, not x86, so, fallback to the automated builds on my x86 box... (Or use http://code.google.com/p/mingw-w64-dgn/)
+## but they're actually for an x86_64 Linux host, not x86, so, fallback to the automated builds on my x86 box... (Or use http://sourceforge.net/projects/mingw-w64-dgn/)
 
 # Make sure we're up to date
 git pull
