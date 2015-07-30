19ddcff55d7412294905620a924d56dfe37b38d2
NiLuJe <ninuje@gmail.com>
2015-07-30 08:02:37 +0200

    This is not hex anymore...
    Potentially base32hex, maybe even base36?

--- tools/kindle_model_sort.py
+++ tools/kindle_model_sort.py
@@ -56,13 +56,13 @@ model_tuples = [
 	('KindlePaperWhite3', 0x90),
 	('KindleUnknown', 0x00)
 ]
-# FIXME: PW3 is inaccurate, the device id potentially moved 2 chars to the right? First char (?) often seems to be 9? Might we sometime find our good old B there, too?
-#('KindlePaperWhite3Wifi_0x0G1', 0x?0G1, 'A21RY355YUXQAF')
-#('KindlePaperWhite3Unknown_0x0G2', 0x?0G2, 'A6S0KGW65V1TV')
-#('KindlePaperWhite3Unknown_0x0G4', 0x?0G4, 'A3P87LH4DLAKE2')
-#('KindlePaperWhite3Unknown_0x0G5', 0x?0G5, 'A3OLIINW419WLP')
-#('KindlePaperWhite3Unknown_0x0G6', 0x?0G6, 'AOPKCG97868D2')
-#('KindlePaperWhite3Unknown_0x0G7', 0x?0G7, 'A3MTNJ7FDYZOPO')
+# FIXME: PW3 is inaccurate, the device id potentially moved 2 chars to the right? First char (?) often (always?) seems to be 9? Might we sometime find our good old B there, too?
+#('KindlePaperWhite3Wifi_0G1', ?0G1, 'A21RY355YUXQAF')
+#('KindlePaperWhite3Unknown_0G2', ?0G2, 'A6S0KGW65V1TV')
+#('KindlePaperWhite3Unknown_0G4', ?0G4, 'A3P87LH4DLAKE2')
+#('KindlePaperWhite3Unknown_0G5', ?0G5, 'A3OLIINW419WLP')
+#('KindlePaperWhite3Unknown_0G6', ?0G6, 'AOPKCG97868D2')
+#('KindlePaperWhite3Unknown_0G7', ?0G7, 'A3MTNJ7FDYZOPO')
 
 
 print 'Kindle models sorted by device code\n'
