f067e4bb7755f1a255ea945221f89a9112b1c5c0
NiLuJe <ninuje@gmail.com>
2015-09-26 14:20:18 +0200

    Forgot to commit the split headers...

---
+++ KindleTool/convert.h
@@ -0,0 +1,37 @@
+//
+//  kindle_tool.h
+//  KindleTool
+//
+//  Copyright (C) 2011-2015  Yifan Lu
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef KINDLECONVERT
+#define KINDLECONVERT
+
+static const char *convert_magic_number(char *);
+static char *to_base(int64_t, unsigned int);
+static int kindle_read_bundle_header(UpdateHeader *, FILE *);
+static int kindle_convert(FILE *, FILE *, FILE *, const unsigned int, const unsigned int, FILE *, char *);
+static int kindle_convert_ota_update_v2(FILE *, FILE *, const unsigned int, char *);
+static int kindle_convert_signature(UpdateHeader *, FILE *, FILE *);
+static int kindle_convert_ota_update(UpdateHeader *, FILE *, FILE *, const unsigned int, char *);
+static int kindle_convert_recovery(UpdateHeader *, FILE *, FILE *, const unsigned int, char *);
+static int kindle_convert_recovery_v2(FILE *, FILE *, const unsigned int, char *);
+static int libarchive_extract(const char *, const char *);
+
+#endif
+
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on;
---
+++ KindleTool/create.h
@@ -0,0 +1,80 @@
+//
+//  kindle_tool.h
+//  KindleTool
+//
+//  Copyright (C) 2011-2015  Yifan Lu
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef KINDLECREATE
+#define KINDLECREATE
+
+typedef struct
+{
+    char magic_number[MAGIC_NUMBER_LENGTH];
+    BundleVersion version;
+    struct rsa_private_key sign_pkey;
+    uint64_t source_revision;
+    uint64_t target_revision;
+    uint32_t magic_1;
+    uint32_t magic_2;
+    uint32_t minor;
+    uint16_t num_devices;
+    Device *devices;
+    Platform platform;
+    Board board;
+    uint32_t header_rev;
+    CertificateNumber certificate_number;
+    unsigned char optional;
+    unsigned char critical;
+    uint16_t num_meta;
+    char **metastrings;
+} UpdateInformation;
+
+// This is modeled after libarchive's bsdtar...
+struct kttar
+{
+    unsigned char *buff;
+    size_t buff_size;
+    char **to_sign_and_bundle_list;
+    char **tweaked_to_sign_and_bundle_list;
+    unsigned int sign_and_bundle_index;
+    unsigned int has_script;
+    size_t tweak_pointer_index;
+};
+
+static const char *convert_bundle_version(BundleVersion);
+
+static struct rsa_private_key get_default_key(void);
+static int sign_file(FILE *, struct rsa_private_key *, FILE *);
+
+static int metadata_filter(struct archive *, void *, struct archive_entry *);
+static int write_file(struct kttar *, struct archive *, struct archive *, struct archive_entry *);
+static int write_entry(struct kttar *, struct archive *, struct archive *, struct archive_entry *);
+static int copy_file_data_block(struct kttar *, struct archive *, struct archive *, struct archive_entry *);
+
+static int create_from_archive_read_disk(struct kttar *, struct archive *, char *, int, char *, const unsigned int);
+
+static int kindle_create_package_archive(const int, char **, const unsigned int, struct rsa_private_key *, const unsigned int, const unsigned int);
+static int kindle_create(UpdateInformation *, FILE *, FILE *, const unsigned int);
+static int kindle_create_ota_update_v2(UpdateInformation *, FILE *, FILE *, const unsigned int);
+static int kindle_create_signature(UpdateInformation *, FILE *, FILE *);
+static int kindle_create_ota_update(UpdateInformation *, FILE *, FILE *, const unsigned int);
+static int kindle_create_recovery(UpdateInformation *, FILE *, FILE *, const unsigned int);
+static int kindle_create_recovery_v2(UpdateInformation *, FILE *, FILE *, const unsigned int);
+
+#endif
+
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on;
