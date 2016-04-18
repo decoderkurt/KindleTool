ab4da40f5f898a70e195b515a16e19184d4a7c7d
NiLuJe <ninuje@gmail.com>
2016-04-18 19:41:36 +0200

    Make sure kt_win_tmpfile creates true temp files...
    Unlike mkstemp, we do expect those to vanish on close ;).

--- KindleTool/kindle_tool.h
+++ KindleTool/kindle_tool.h
@@ -118,7 +118,7 @@ static inline FILE *kt_win_tmpfile(void)
         fprintf(stderr, "Couldn't create temporary file template: %s.\n", strerror(errno));
         return NULL;
     }
-    int fd = _open(template, _O_CREAT | _O_EXCL | _O_RDWR | _O_BINARY, _S_IREAD | _S_IWRITE);
+    int fd = _open(template, _O_CREAT | _O_EXCL | _O_RDWR | _O_BINARY | _O_TEMPORARY, _S_IREAD | _S_IWRITE);
     if(fd == -1)
     {
         fprintf(stderr, "Couldn't open temporary file: %s.\n", strerror(errno));
