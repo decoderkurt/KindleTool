58a9989bcca4115cf012fe8f1d28ec5e6d524ed4
NiLuJe <ninuje@gmail.com>
2016-06-21 18:21:27 +0200

    Bump gmp for binary builds

--- tools/simple-linux-static-build.sh
+++ tools/simple-linux-static-build.sh
@@ -22,11 +22,11 @@ Build_Linux() {
 		GMPABI="32"
 	fi
 
-	GMP_VER="6.1.0"
+	GMP_VER="6.1.1"
 	GMP_DIR="gmp-${GMP_VER%a}"
-	NETTLE_VER="3.1.1"
+	NETTLE_VER="3.2"
 	NETTLE_DIR="nettle-${NETTLE_VER}"
-	LIBARCHIVE_VER="3.1.2"
+	LIBARCHIVE_VER="3.2.1"
 	LIBARCHIVE_DIR="libarchive-${LIBARCHIVE_VER}"
 
 	# Make sure we're up to date
