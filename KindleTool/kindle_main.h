f045a8ee164559ea5a2f4a82b0be37fc6e8e7bd2
NiLuJe <ninuje@gmail.com>
2018-04-20 06:35:56 +0200

    This finally stops KDevelop's static analyzer from losing its shit...
    (_GNU_SOURCE & linux/limits.h)

--- KindleTool/kindle_main.h
+++ KindleTool/kindle_main.h
@@ -1,25 +1,24 @@
-//
-//  kindle_main.h
-//  KindleTool
-//
-//  Copyright (C) 2011-2012  Yifan Lu
-//  Copyright (C) 2012-2018  NiLuJe
-//  Concept based on an original Python implementation by Igor Skochinsky & Jean-Yves Avenard,
-//    cf., http://www.mobileread.com/forums/showthread.php?t=63225
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+/*
+**  KindleTool, kindle_main.h
+**
+**  Copyright (C) 2011-2012  Yifan Lu
+**  Copyright (C) 2012-2018  NiLuJe
+**  Concept based on an original Python implementation by Igor Skochinsky & Jean-Yves Avenard,
+**    cf., http://www.mobileread.com/forums/showthread.php?t=63225
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef __KINDLETOOL_MAIN_H
 #define __KINDLETOOL_MAIN_H
--- KindleTool/kindle_tool.h
+++ KindleTool/kindle_tool.h
@@ -1,28 +1,33 @@
-//
-//  kindle_tool.h
-//  KindleTool
-//
-//  Copyright (C) 2011-2012  Yifan Lu
-//  Copyright (C) 2012-2018  NiLuJe
-//  Concept based on an original Python implementation by Igor Skochinsky & Jean-Yves Avenard,
-//    cf., http://www.mobileread.com/forums/showthread.php?t=63225
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+/*
+**  KindleTool, kindle_tool.h
+**
+**  Copyright (C) 2011-2012  Yifan Lu
+**  Copyright (C) 2012-2018  NiLuJe
+**  Concept based on an original Python implementation by Igor Skochinsky & Jean-Yves Avenard,
+**    cf., http://www.mobileread.com/forums/showthread.php?t=63225
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef __KINDLETOOL_H
 #define __KINDLETOOL_H
+
+#if defined(__linux__)
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#endif
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -35,6 +40,9 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include <limits.h>
+#if defined(__linux__)
+#include <linux/limits.h>
+#endif
 #include <libgen.h>
 
 // libarchive does not pull that in for us anymore ;).
