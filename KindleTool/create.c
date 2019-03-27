98f65e8c722bdaa05273a6050e18ce20d8ee44d8
NiLuJe <ninuje@gmail.com>
2019-03-27 17:17:36 +0100

    Bump enforce_ota to 5.11.1

--- KindleTool/create.c
+++ KindleTool/create.c
@@ -2354,7 +2354,7 @@ int
 				info.source_revision = 2443670049;    // FW 5.5.0
 			}
 			if (!enforce_target_rev) {
-				info.target_revision = 1 + 3383210041;    // FW 5.10.3 (KOA2)
+				info.target_revision = 1 + 3527990035;    // FW 5.11.1 (PW4)
 			}
 			// NOTE: Don't expect those to be entirely consistent when crossing devices
 			//       (f.g., the Touch's FW 5.3.7.3 has a higher OTA build number than the KV's FW 5.5.0)
