389e5b71d7efad50c9f8ad38c9ca827a9bed9d5f
NiLuJe <ninuje@gmail.com>
2019-08-02 02:12:01 +0200

    Squish warnings

--- KindleTool/kindle_tool.c
+++ KindleTool/kindle_tool.c
@@ -41,29 +41,29 @@ FILE*
 
 	// Now, because the CRT's _mktemp is terrible (PID based), duplicate MinGW's mkstemp logic...
 	// c.f., https://sourceforge.net/p/mingw-w64/mingw-w64/ci/master/tree/mingw-w64-crt/misc/mkstemp.c
-	int i, j, index, len;
+	size_t index, len;
 
 	/* These are the (62) characters used in temporary filenames. */
 	static const char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 	/* The last six characters of template must be "XXXXXX" */
-	if (template == NULL || (len = strlen(template)) < 6 || memcmp(template + (len - 6), "XXXXXX", 6)) {
+	if (template == NULL || (len = strlen(template)) < 6U || memcmp(template + (len - 6U), "XXXXXX", 6U)) {
 		errno = EINVAL;
 		fprintf(stderr, "Couldn't create temporary file template: %s.\n", strerror(errno));
 		return NULL;
 	}
 
 	/* User may supply more than six trailing Xs */
-	for (index = len - 6; index > 0 && template[index - 1] == 'X'; index--)
+	for (index = len - 6U; index > 0U && template[index - 1U] == 'X'; index--)
 		;
 
 	/*
 	Like OpenBSD, mkstemp() will try at least 2 ** 31 combinations before
 	giving up.
 	*/
-	for (i = 0; i >= 0; i++) {
-		for (j = index; j < len; j++) {
-			template[j] = letters[rand() % 62];
+	for (int i = 0; i >= 0; i++) {
+		for (size_t j = index; j < len; j++) {
+			template[j] = letters[rand() % 62U];
 		}
 		// NOTE: And we can't use mkstemp directly, because we *DO* want O_TEMPORARY here...
 		fd = _sopen(
