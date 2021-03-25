090a2d3afe6e80d63185a036959ecba52b4bdc49
NiLuJe <ninuje@gmail.com>
2021-03-25 18:20:38 +0100

    FW 5.12.2.1.1 is the gift that keeps on giving...

--- KindleTool/create.c
+++ KindleTool/create.c
@@ -2491,16 +2491,18 @@ int
 			memcpy(info.magic_number, "FC04", MAGIC_NUMBER_LENGTH);
 			// But also a source & target version!
 			if (!enforce_source_rev) {
-				info.source_revision = 2443670049;    // FW 5.5.0
+				info.source_revision = 2443670049U;    // FW 5.5.0
 			}
 			if (!enforce_target_rev) {
-				info.target_revision = 3720580001;    // FW 5.12.2.1.1 (PW2 & KT2)
+				info.target_revision = 3720580001U + 1U;    // FW 5.12.2.1.1 (PW2 & KT2)
+
 			}
 			// NOTE: Don't expect those to be entirely consistent when crossing devices
 			//       (f.g., the Touch's FW 5.3.7.3 has a higher OTA build number than the KV's FW 5.5.0)
 			// NOTE: The VoiceView packages may sometimes have a slightly higher Target OTA flag, f.g.,
 			//       while FW 5.10.1.2 was at 3360340004, the PW4 VoiceView packages targeted 3372830002,
 			//       which turned out to be FW 5.10.1.3
+			// NOTE: In a fun, new, twist, FW 5.12.2.1.1 appears to prevent same-to-same updates, hence the + 1...
 		}
 		// Musn't be *only* a sig envelope...
 		if (info.version == UpdateSignature) {
