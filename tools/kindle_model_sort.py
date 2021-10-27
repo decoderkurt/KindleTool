5ed10ef0427a2339b19ba7765eabbf24bcc0c5d0
NiLuJe <ninuje@gmail.com>
2021-10-27 22:35:47 +0200

    Start adding PW5 devices IDs

--- tools/kindle_model_sort.py
+++ tools/kindle_model_sort.py
@@ -35,127 +35,135 @@ def devCode(string):
 
 
 model_tuples = [
-	('Kindle1', 0x01, 'ATVPDKIKX0DER'),
-	('Kindle2US', 0x02, 'A3UN6WX5RRO2AG'),
-	('Kindle2International', 0x03, 'A1F83G8C2ARO7P'),
-	('KindleDXUS', 0x04, 'A1PA6795UKMFR9'),
-	('KindleDXInternational', 0x05, 'A13V1IB3VIYZZH'),
-	('ValidKindleUnknown_0x07', 0x07, 'A2EUQ1WTGCTBG2'),
-	('Kindle3WiFi3G', 0x06, 'A1VC38T7YXB528'),
-	('Kindle3WiFi', 0x08, 'A3AEGXETSR30VB'),
-	('KindleDXGraphite', 0x09, 'A3P5ROKL5A1OLE'),
-	('Kindle3WiFi3GEurope', 0x0A, 'A3JWKAKR8XB7XF'),
-	('ValidKindleUnknown_0x0B', 0x0B, 'A1X6FK5RDHNB96'),
-	('ValidKindleUnknown_0x0C', 0x0C, 'AN1VRQENFRJN5'),
-	('ValidKindleUnknown_0x0D', 0x0D, 'A3DWYIK6Y9EEQB'),
-	('Kindle4NonTouch', 0x0E, 'A3R76HOPU0Z2CB'),
-	('Kindle5TouchWiFi3G', 0x0F, 'A1IM4EOPHS76S7'),
-	('Kindle5TouchWiFi3GEurope', 0x10, 'A138L1TOL8PIJT'),
-	('Kindle5TouchWiFi', 0x11, 'A3T4TT2Z381HKD'),
-	('Kindle5TouchUnknown', 0x12, 'A3LJ5WMKNRFKQS'),
-	('KindlePaperWhiteWiFi3G', 0x1B, 'A1JYRMDPD0WRC1'),
-	('KindlePaperWhiteWiFi3GCanada', 0x1C, 'A1U5RCOVU0NYF2'),
-	('KindlePaperWhiteWiFi3GEurope', 0x1D, 'A1I7TFXKDRQDZL'),
-	('KindlePaperWhiteWiFi3GJapan', 0x1F, 'A1K21FY43GMZF8'),
-	('KindlePaperWhiteWiFi3GBrazil', 0x20, 'A3RN7G7QC5MWSZ'),
-	('Kindle4NonTouchBlack', 0x23),
-	('KindlePaperWhiteWiFi', 0x24, 'A3VSAZHKW7EWVH'),
-	('KindlePaperWhite2WiFiJapan', 0x5A, 'A1XFE4LQM16OSW'),
-	('KindlePaperWhite2WiFi', 0xD4, 'A2X1JOFWQIYV75'),
-	('KindlePaperWhite2WiFi3G', 0xD5, 'A2LTUGSV2JQ93O'),
-	('KindlePaperWhite2WiFi3GCanada', 0xD6, 'A3CG2RMGG8NQEJ'),
-	('KindlePaperWhite2WiFi3GEurope', 0xD7, 'A2RWEQK36M6DUE'),
-	('KindlePaperWhite2WiFi3GRussia', 0xD8, 'A3DM9ZTSZGUSMW'),
-	('KindlePaperWhite2WiFi3GJapan', 0xF2, 'A36L7QE2V0XKCZ'),
-	('KindlePaperWhite2WiFi4GBInternational', 0x17, 'A3I3CR3NPZFVHY'),
-	('KindlePaperWhite2WiFi3G4GBCanada', 0x5F, 'A16EMENY0O3Z2H'),
-	('KindlePaperWhite2WiFi3G4GBEurope', 0x60, 'A3D1N3J5SXSYPF'),
-	('KindlePaperWhite2WiFi3G4GBBrazil', 0x61, 'A3NRQ2KXEO33BF'),
-	('KindlePaperWhite2WiFi3G4GB', 0x62, 'A3QT0UFVNUDPAE'),
-	('KindlePaperWhite2Unknown_0xF4', 0xF4),
-	('KindlePaperWhite2Unknown_0xF9', 0xF9),
-	('KindleVoyageWiFi', 0x13, 'A3FE7AD5N5R11'),
-	('KindleVoyageWiFi3G', 0x54, 'A1VHVRSIVA49BF'),
-	('KindleVoyageWiFi3GJapan', 0x2A, 'A2KSI370ME58SV'),
-	('KindleVoyageUnknown_0x4F', 0x4F, 'AEK24W3B90XSI'),
-	('KindleVoyageWiFi3GMexico', 0x52, 'A66ZTOXC8UWFP'),
-	('KindleVoyageWiFi3GEurope', 0x53, 'A26JMGYIXWMKGL'),
-	('KindleBasic', 0xC6, 'A2TNPB8EVLW5FA'),
-	('ValidKindleUnknown_0x99', 0x99, 'A2I96HKA5TK143'),
-	('KindleBasicKiwi', 0xDD, 'A9N06WOIL49CA'),
-	('ValidKindleUnknown_0x16', 0x16),
-	('ValidKindleUnknown_0x21', 0x21),
-	('KindlePaperWhite3WiFi', 0x201, 'A21RY355YUXQAF'),		# 0G1
-	('KindlePaperWhite3WiFi3G', 0x202, 'A6S0KGW65V1TV'),		# 0G2
-	('KindlePaperWhite3WiFi3GMexico', 0x204, 'A3P87LH4DLAKE2'),	# 0G4
-	('KindlePaperWhite3WiFi3GEurope', 0x205, 'A3OLIINW419WLP'),	# 0G5
-	('KindlePaperWhite3WiFi3GCanada', 0x206, 'AOPKCG97868D2'),	# 0G6
-	('KindlePaperWhite3WiFi3GJapan', 0x207, 'A3MTNJ7FDYZOPO'),	# 0G7
-	('KindlePaperWhite3WhiteWiFi', 0x26B),				# 0KB
-	('KindlePaperWhite3WhiteWiFi3GJapan', 0x26C),			# 0KC
-	('KindlePW3WhiteUnknown_0KD', 0x26D),				# 0KD?
-	('KindlePaperWhite3WhiteWiFi3GInternational', 0x26E),		# 0KE
-	('KindlePaperWhite3WhiteWiFi3GInternationalBis', 0x26F),	# 0KF
-	('KindlePW3WhiteUnknown_0KG', 0x270),				# 0KG?
-	('KindlePaperWhite3WiFi32GBJapanBlack', 0x293),			# 0LK
-	('KindlePaperWhite3WiFi32GBJapanWhite', 0x294),			# 0LL
-	('KindleOasisWiFi', 0x20C),					# 0GC
-	('KindleOasisWiFi3G', 0x20D),					# 0GD
-	('KindleOasisWiFi3GInternational', 0x219),			# 0GR
-	('KindleOasisUnknown_0GS', 0x21A),				# 0GS?
-	('KindleOasisWiFi3GChina', 0x21B),				# 0GT
-	('KindleOasisWiFi3GEurope', 0x21C),				# 0GU
-	('KindleBasic2Unknown_0DU', 0x1BC),				# 0DU?
-	('KindleBasic2', 0x269),					# 0K9
-	('KindleBasic2White', 0x26A),					# 0KA
-	('KindleOasis2Unknown_0LM', 0x295),				# 0LM?
-	('KindleOasis2Unknown_0LN', 0x296),				# 0LN?
-	('KindleOasis2Unknown_0LP', 0x297),				# 0LP?
-	('KindleOasis2Unknown_0LQ', 0x298),				# 0LQ?
-	('KindleOasis2WiFi32GBChampagne', 0x2E1),			# 0P1
-	('KindleOasis2Unknown_0P2', 0x2E2),				# 0P2?
-	('KindleOasis2Unknown_0P6', 0x2E6),				# 0P6
-	('KindleOasis2Unknown_0P7', 0x2E7),				# 0P7?
-	('KindleOasis2WiFi8GB', 0x2E8),					# 0P8
-	('KindleOasis2WiFi3G32GB', 0x341),				# 0S1
-	('KindleOasis2WiFi3G32GBEurope', 0x342),			# 0S2
-	('KindleOasis2Unknown_0S3', 0x343),				# 0S3?
-	('KindleOasis2Unknown_0S4', 0x344),				# 0S4?
-	('KindleOasis2Unknown_0S7', 0x347),				# 0S7?
-	('KindleOasis2WiFi32GB', 0x34A),				# 0SA
-	('KindlePaperWhite4WiFi8GB', 0x2F7, 'AJRLVDTOPT1LE'),		# 0PP
-	('KindlePaperWhite4WiFi4G32GB', 0x361),				# 0T1
-	('KindlePaperWhite4WiFi4G32GBEurope', 0x362),			# 0T2
-	('KindlePaperWhite4WiFi4G32GBJapan', 0x363),			# 0T3
-	('KindlePaperWhite4Unknown_0T4', 0x364),			# 0T4?
-	('KindlePaperWhite4Unknown_0T5', 0x365),			# 0T5?
-	('KindlePaperWhite4WiFi32GB', 0x366),				# 0T6
-	('KindlePaperWhite4Unknown_0T7', 0x367),			# 0T7?
-	('KindlePaperWhite4Unknown_0TJ', 0x372),			# 0TJ?
-	('KindlePaperWhite4Unknown_0TK', 0x373),			# 0TK?
-	('KindlePaperWhite4Unknown_0TL', 0x374),			# 0TL?
-	('KindlePaperWhite4Unknown_0TM', 0x375),			# 0TM?
-	('KindlePaperWhite4Unknown_0TN', 0x376),			# 0TN?
-	('KindlePaperWhite4WiFi8GBIndia', 0x402),			# 102
-	('KindlePaperWhite4WiFi32GBIndia', 0x403),			# 103
-	('KindlePaperWhite4WiFi32GBBlue', 0x4D8),			# 16Q
-	('KindlePaperWhite4WiFi32GBPlum', 0x4D9),			# 16R
-	('KindlePaperWhite4WiFi32GBSage', 0x4DA),			# 16S
-	('KindlePaperWhite4WiFi8GBBlue', 0x4DB),			# 16T
-	('KindlePaperWhite4WiFi8GBPlum', 0x4DC),			# 16U
-	('KindlePaperWhite4WiFi8GBSage', 0x4DD),			# 16V
-	('KindleBasic3', 0x414),					# 10L
-	('KindleBasic3White8GB', 0x3CF),				# 0WF
-	('KindleBasic3Unknown_0WG', 0x3D0),				# 0WG?
-	('KindleBasic3White', 0x3D1),					# 0WH
-	('KindleBasic3Unknown_0WJ', 0x3D2),				# 0WJ?
-	('KindleBasic3KidsEdition', 0x3AB),				# 0VB
-	('KindleOasis3WiFi32GBChampagne', 0x434),			# 11L
-	('KindleOasis3WiFi4G32GBJapan', 0x3D8),				# 0WQ
-	('KindleOasis3WiFi4G32GBIndia', 0x3D7),				# 0WP
-	('KindleOasis3WiFi4G32GB', 0x3D6),				# 0WN
-	('KindleOasis3WiFi32GB', 0x3D5),				# 0WM
-	('KindleOasis3WiFi8GB', 0x3D4),					# 0WL
+	('Kindle1',						0x01,	'ATVPDKIKX0DER'),
+	('Kindle2US',						0x02,	'A3UN6WX5RRO2AG'),
+	('Kindle2International',				0x03,	'A1F83G8C2ARO7P'),
+	('KindleDXUS',						0x04,	'A1PA6795UKMFR9'),
+	('KindleDXInternational',				0x05,	'A13V1IB3VIYZZH'),
+	('ValidKindleUnknown_0x07',				0x07,	'A2EUQ1WTGCTBG2'),
+	('Kindle3WiFi3G',					0x06,	'A1VC38T7YXB528'),
+	('Kindle3WiFi',						0x08,	'A3AEGXETSR30VB'),
+	('KindleDXGraphite',					0x09,	'A3P5ROKL5A1OLE'),
+	('Kindle3WiFi3GEurope',					0x0A,	'A3JWKAKR8XB7XF'),
+	('ValidKindleUnknown_0x0B',				0x0B,	'A1X6FK5RDHNB96'),
+	('ValidKindleUnknown_0x0C',				0x0C,	'AN1VRQENFRJN5'),
+	('ValidKindleUnknown_0x0D',				0x0D,	'A3DWYIK6Y9EEQB'),
+	('Kindle4NonTouch',					0x0E,	'A3R76HOPU0Z2CB'),
+	('Kindle5TouchWiFi3G',					0x0F,	'A1IM4EOPHS76S7'),
+	('Kindle5TouchWiFi3GEurope',				0x10,	'A138L1TOL8PIJT'),
+	('Kindle5TouchWiFi',					0x11,	'A3T4TT2Z381HKD'),
+	('Kindle5TouchUnknown',					0x12,	'A3LJ5WMKNRFKQS'),
+	('KindlePaperWhiteWiFi3G',				0x1B,	'A1JYRMDPD0WRC1'),
+	('KindlePaperWhiteWiFi3GCanada',			0x1C,	'A1U5RCOVU0NYF2'),
+	('KindlePaperWhiteWiFi3GEurope',			0x1D,	'A1I7TFXKDRQDZL'),
+	('KindlePaperWhiteWiFi3GJapan',				0x1F,	'A1K21FY43GMZF8'),
+	('KindlePaperWhiteWiFi3GBrazil',			0x20,	'A3RN7G7QC5MWSZ'),
+	('Kindle4NonTouchBlack',				0x23),
+	('KindlePaperWhiteWiFi',				0x24,	'A3VSAZHKW7EWVH'),
+	('KindlePaperWhite2WiFiJapan',				0x5A,	'A1XFE4LQM16OSW'),
+	('KindlePaperWhite2WiFi',				0xD4,	'A2X1JOFWQIYV75'),
+	('KindlePaperWhite2WiFi3G',				0xD5,	'A2LTUGSV2JQ93O'),
+	('KindlePaperWhite2WiFi3GCanada',			0xD6,	'A3CG2RMGG8NQEJ'),
+	('KindlePaperWhite2WiFi3GEurope',			0xD7,	'A2RWEQK36M6DUE'),
+	('KindlePaperWhite2WiFi3GRussia',			0xD8,	'A3DM9ZTSZGUSMW'),
+	('KindlePaperWhite2WiFi3GJapan',			0xF2,	'A36L7QE2V0XKCZ'),
+	('KindlePaperWhite2WiFi4GBInternational',		0x17,	'A3I3CR3NPZFVHY'),
+	('KindlePaperWhite2WiFi3G4GBCanada',			0x5F,	'A16EMENY0O3Z2H'),
+	('KindlePaperWhite2WiFi3G4GBEurope',			0x60,	'A3D1N3J5SXSYPF'),
+	('KindlePaperWhite2WiFi3G4GBBrazil',			0x61,	'A3NRQ2KXEO33BF'),
+	('KindlePaperWhite2WiFi3G4GB',				0x62,	'A3QT0UFVNUDPAE'),
+	('KindlePaperWhite2Unknown_0xF4',			0xF4,	'A3JI3C11GUW6OM'),
+	('KindlePaperWhite2Unknown_0xF9',			0xF9,	'A148QFVDZ3MQ8V'),
+	('KindleVoyageWiFi',					0x13,	'A3FE7AD5N5R11'),
+	('KindleVoyageWiFi3G',					0x54,	'A1VHVRSIVA49BF'),
+	('KindleVoyageWiFi3GJapan',				0x2A,	'A2KSI370ME58SV'),
+	('KindleVoyageUnknown_0x4F',				0x4F,	'AEK24W3B90XSI'),
+	('KindleVoyageWiFi3GMexico',				0x52,	'A66ZTOXC8UWFP'),
+	('KindleVoyageWiFi3GEurope',				0x53,	'A26JMGYIXWMKGL'),
+	('KindleBasic',						0xC6,	'A2TNPB8EVLW5FA'),
+	('ValidKindleUnknown_0x99',				0x99,	'A2I96HKA5TK143'),
+	('KindleBasicKiwi',					0xDD,	'A9N06WOIL49CA'),
+	('ValidKindleUnknown_0x16',				0x16),
+	('ValidKindleUnknown_0x21',				0x21),
+	('KindlePaperWhite3WiFi',				0x201,	'A21RY355YUXQAF'),	# 0G1
+	('KindlePaperWhite3WiFi3G',				0x202,	'A6S0KGW65V1TV'),	# 0G2
+	('KindlePaperWhite3WiFi3GMexico',			0x204,	'A3P87LH4DLAKE2'),	# 0G4
+	('KindlePaperWhite3WiFi3GEurope',			0x205,	'A3OLIINW419WLP'),	# 0G5
+	('KindlePaperWhite3WiFi3GCanada',			0x206,	'AOPKCG97868D2'),	# 0G6
+	('KindlePaperWhite3WiFi3GJapan',			0x207,	'A3MTNJ7FDYZOPO'),	# 0G7
+	('KindlePaperWhite3WhiteWiFi',				0x26B,	'A21RY355YUXQAF'),	# 0KB
+	('KindlePaperWhite3WhiteWiFi3GJapan',			0x26C,	'A3MTNJ7FDYZOPO'),	# 0KC
+	('KindlePW3WhiteUnknown_0KD',				0x26D,	'AOPKCG97868D2'),	# 0KD?
+	('KindlePaperWhite3WhiteWiFi3GInternational',		0x26E,	'A3OLIINW419WLP'),	# 0KE
+	('KindlePaperWhite3WhiteWiFi3GInternationalBis',	0x26F,	'A6S0KGW65V1TV'),	# 0KF
+	('KindlePW3WhiteUnknown_0KG',				0x270,	'A3P87LH4DLAKE2'),	# 0KG?
+	('KindlePaperWhite3WiFi32GBJapanBlack',			0x293,	'A2T9E09EBKRBWU'),	# 0LK
+	('KindlePaperWhite3WiFi32GBJapanWhite',			0x294,	'A2T9E09EBKRBWU'),	# 0LL
+	('KindleOasisWiFi',					0x20C,	'A2NP90AR02CXEG'),	# 0GC
+	('KindleOasisWiFi3G',					0x20D,	'A370DV3BFIHFD3'),	# 0GD
+	('KindleOasisWiFi3GInternational',			0x219,	'A21R12JDS0I7HR'),	# 0GR
+	('KindleOasisUnknown_0GS',				0x21A,	'A2G9XCYZJMNLQK'),	# 0GS?
+	('KindleOasisWiFi3GChina',				0x21B,	'AIOUHGSC1FXK5'),	# 0GT
+	('KindleOasisWiFi3GEurope',				0x21C,	'A1VYPQEAEVB479'),	# 0GU
+	('KindleBasic2Unknown_0DU',				0x1BC),				# 0DU?
+	('KindleBasic2',					0x269,	'A363JBKK6AP29Q'),	# 0K9
+	('KindleBasic2White',					0x26A,	'A363JBKK6AP29Q'),	# 0KA
+	('KindleOasis2Unknown_0LM',				0x295,	'A2AVNKP6ZINL5'),	# 0LM?
+	('KindleOasis2Unknown_0LN',				0x296,	'A1SZ6LXIZK7826'),	# 0LN?
+	('KindleOasis2Unknown_0LP',				0x297,	'A3M646A6GS49CA'),	# 0LP?
+	('KindleOasis2Unknown_0LQ',				0x298,	'A39S6AFBERWZOH'),	# 0LQ?
+	('KindleOasis2WiFi32GBChampagne',			0x2E1,	'A1SZ6LXIZK7826'),	# 0P1
+	('KindleOasis2Unknown_0P2',				0x2E2,	'A2AVNKP6ZINL5'),	# 0P2?
+	('KindleOasis2Unknown_0P6',				0x2E6,	'A3M646A6GS49CA'),	# 0P6
+	('KindleOasis2Unknown_0P7',				0x2E7,	'A39S6AFBERWZOH'),	# 0P7?
+	('KindleOasis2WiFi8GB',					0x2E8,	'A1SZ6LXIZK7826'),	# 0P8
+	('KindleOasis2WiFi3G32GB',				0x341,	'A2AVNKP6ZINL5'),	# 0S1
+	('KindleOasis2WiFi3G32GBEurope',			0x342,	'A3M646A6GS49CA'),	# 0S2
+	('KindleOasis2Unknown_0S3',				0x343,	'A39S6AFBERWZOH'),	# 0S3?
+	('KindleOasis2Unknown_0S4',				0x344,	'A1SZ6LXIZK7826'),	# 0S4?
+	('KindleOasis2Unknown_0S7',				0x347,	'A1SZ6LXIZK7826'),	# 0S7?
+	('KindleOasis2WiFi32GB',				0x34A,	'A1SZ6LXIZK7826'),	# 0SA
+	('KindlePaperWhite4WiFi8GB',				0x2F7,	'AJRLVDTOPT1LE'),	# 0PP
+	('KindlePaperWhite4WiFi4G32GB',				0x361,	'A3IT5K46YEJ8DG'),	# 0T1
+	('KindlePaperWhite4WiFi4G32GBEurope',			0x362,	'A2J0U8ZY7AYQWV'),	# 0T2
+	('KindlePaperWhite4WiFi4G32GBJapan',			0x363,	'AV9Q59KU8EJQE'),	# 0T3
+	('KindlePaperWhite4Unknown_0T4',			0x364,	'A27ME72Q2PS699'),	# 0T4?
+	('KindlePaperWhite4Unknown_0T5',			0x365,	'A3IT5K46YEJ8DG'),	# 0T5?
+	('KindlePaperWhite4WiFi32GB',				0x366,	'AJRLVDTOPT1LE'),	# 0T6
+	('KindlePaperWhite4Unknown_0T7',			0x367,	'AJRLVDTOPT1LE'),	# 0T7?
+	('KindlePaperWhite4Unknown_0TJ',			0x372,	'AJRLVDTOPT1LE'),	# 0TJ?
+	('KindlePaperWhite4Unknown_0TK',			0x373,	'AJRLVDTOPT1LE'),	# 0TK?
+	('KindlePaperWhite4Unknown_0TL',			0x374,	'A2J0U8ZY7AYQWV'),	# 0TL?
+	('KindlePaperWhite4Unknown_0TM',			0x375,	'AV9Q59KU8EJQE'),	# 0TM?
+	('KindlePaperWhite4Unknown_0TN',			0x376,	'A27ME72Q2PS699'),	# 0TN?
+	('KindlePaperWhite4WiFi8GBIndia',			0x402,	'AJRLVDTOPT1LE'),	# 102
+	('KindlePaperWhite4WiFi32GBIndia',			0x403,	'A2J0U8ZY7AYQWV'),	# 103
+	('KindlePaperWhite4WiFi32GBBlue',			0x4D8,	'AJRLVDTOPT1LE'),	# 16Q
+	('KindlePaperWhite4WiFi32GBPlum',			0x4D9,	'AJRLVDTOPT1LE'),	# 16R
+	('KindlePaperWhite4WiFi32GBSage',			0x4DA,	'AJRLVDTOPT1LE'),	# 16S
+	('KindlePaperWhite4WiFi8GBBlue',			0x4DB,	'AJRLVDTOPT1LE'),	# 16T
+	('KindlePaperWhite4WiFi8GBPlum',			0x4DC,	'AJRLVDTOPT1LE'),	# 16U
+	('KindlePaperWhite4WiFi8GBSage',			0x4DD,	'AJRLVDTOPT1LE'),	# 16V
+	('KindleBasic3',					0x414,	'AHU5VU98ZZYIL'),	# 10L
+	('KindleBasic3White8GB',				0x3CF,	'AHU5VU98ZZYIL'),	# 0WF
+	('KindleBasic3Unknown_0WG',				0x3D0,	'AHU5VU98ZZYIL'),	# 0WG?
+	('KindleBasic3White',					0x3D1,	'AHU5VU98ZZYIL'),	# 0WH
+	('KindleBasic3Unknown_0WJ',				0x3D2,	'AHU5VU98ZZYIL'),	# 0WJ?
+	('KindleBasic3KidsEdition',				0x3AB,	'AHU5VU98ZZYIL'),	# 0VB
+	('KindleOasis3WiFi32GBChampagne',			0x434,	'A2NW3VDYR5P8Z0'),	# 11L
+	('KindleOasis3WiFi4G32GBJapan',				0x3D8,	'A28MDQJEP7D12S'),	# 0WQ
+	('KindleOasis3WiFi4G32GBIndia',				0x3D7,	'A2M7UZTFTYKRHM'),	# 0WP
+	('KindleOasis3WiFi4G32GB',				0x3D6,	'AB6KN53ZYVL6D'),	# 0WN
+	('KindleOasis3WiFi32GB',				0x3D5,	'A2NW3VDYR5P8Z0'),	# 0WM
+	('KindleOasis3WiFi8GB',					0x3D4,	'A2NW3VDYR5P8Z0'),	# 0WL
+	#('KindlePaperWhite3Unknown_TTT', 0x??,	'A21RY355YUXQAF') # TTT?
+	#('KindlePaperWhite4Unknown_0PL', 0x??,	'A3IT5K46YEJ8DG') # 0PL?
+	#('KindlePaperWhite5Unknown_1LG', 0x??',	'A328XUBPG464LQ')	# 1LG?
+	#('KindlePaperWhite5Unknown_1Q0', 0x??',	'A328XUBPG464LQ')	# 1Q0?
+	#('KindlePaperWhite5Unknown_1PX', 0x??',	'A328XUBPG464LQ')	# 1PX?
+	#('KindlePaperWhite5Unknown_1VD', 0x??',	'A328XUBPG464LQ')	# 1VD?
+	#('KindlePaperWhite5Unknown_219', 0x??',	'A328XUBPG464LQ')	# 219?
+	#('KindlePaperWhite5Unknown_21A', 0x??',	'A328XUBPG464LQ')	# 21A?
 	('KindleUnknown', 0x00)
 ]
 
